2c867c33eb2e588a21958bed79e837cacbad49be
Priti Roy <roypriti921@gmail.com>
2023-06-24 01:41:28 +0530

    added hero left fruit image

--- fruitopia/components/sections/Hero.jsx
+++ fruitopia/components/sections/Hero.jsx
@@ -6,6 +6,15 @@ const Hero = () => {
     <>
       <div className="background"></div>
       <div>
+        <div className='absolute left-0 mt-3 xl:mt-20 2xl:mt-40'>
+          <Image
+            src="/assets/fruitleft1.png"
+            alt="fruitleft1"
+            height={100}
+            width={100}
+            className='w-[100px] xl:w-[150px] 2xl:w-[200px]'
+          />
+        </div>
         <div className='w-screen flex  flex-col justify-center items-center'>
           <div className="text-center text-white mb-8 2xl:mt-10">
             <h1 className="italic tracking-tight text-2xl xl:text-5xl 2xl:text-7xl">your healthy life</h1>
