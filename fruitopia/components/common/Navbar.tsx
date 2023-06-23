31cc7227a281b1daf07f7c3b46d4c781a599af64
Priti Roy <roypriti921@gmail.com>
2023-06-23 21:28:27 +0530

    fixed laptop screen margin size

--- fruitopia/components/common/Navbar.tsx
+++ fruitopia/components/common/Navbar.tsx
@@ -10,10 +10,10 @@ const Navbar: FC = () => {
 
   return (
     <Container>
-      <div className="flex flex-row sm:mx-72 md:py-10 items-center  antialiased">
+      <div className="flex flex-row xl:mx-44 2xl:mx-72 md:py-10 items-center  antialiased">
 
         {/* nav links */}
-        <nav className="flex justify-start space-x-6 text-base font-semibold capitalize tracking-wider">
+        <nav className="flex justify-start items-center space-x-6 text-base font-semibold capitalize tracking-wider">
           {navLinks.map((link, index) => (
             <Link key={link} href={`/${link}`}>
               <p className={index === 0 ? "text-green-500" : ""}>{link}</p>
@@ -25,10 +25,10 @@ const Navbar: FC = () => {
         <Link href="/" className="text-center w-full text-5xl uppercase text-bold"><h1>fruitopia</h1></Link>
 
         {/* user nav */}
-        <nav className="flex flex-1 justify-end space-x-10 capitalize">
+        <nav className="flex flex-1 justify-end items-center space-x-10 capitalize">
           <p className="px-3 whitespace-nowrap">login / signup</p>
-          <BiSearchAlt size={30} />
-          <FiShoppingBag size={25} />
+          <BiSearchAlt className="text-[20px] 2xl:text-[30px]" />
+          <FiShoppingBag className="text-[20px] 2xl:text-[30px]" />
         </nav>
       </div>
     </Container>
