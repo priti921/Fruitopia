658e502fbc71d1ff388005fa19b45d5d8d3879fd
Priti Roy <roypriti921@gmail.com>
2023-06-24 02:05:22 +0530

    added right fruit image && fixed some issues

--- fruitopia/components/sections/Hero.jsx
+++ fruitopia/components/sections/Hero.jsx
@@ -5,19 +5,31 @@ const Hero = () => {
   return (
     <>
       <div className="background"></div>
-      <div>
-        <div className='absolute left-0 mt-3 xl:mt-20 2xl:mt-40'>
-          <Image
+      {/* HERO LEFT FRUIT */}
+      <div className="w-full mt-20 sm:mt-0">
+        <div className='absolute left-0 mt-3 xl:mt-10 2xl:mt-40'>
+          j  <Image
             src="/assets/fruitleft1.png"
             alt="fruitleft1"
-            height={100}
-            width={100}
-            className='w-[100px] xl:w-[150px] 2xl:w-[200px]'
+            height={2000}
+            width={2000}
+            className='w-[100px] xl:w-[230px] 2xl:w-[270px]'
           />
         </div>
+        {/* HERO RIGHT FRUIT */}
+        <div className="absolute right-0 mt-3 xl:mt-10 2xl:mt-40">
+          <Image
+            src="/assets/fruitsright1.png"
+            alt="fruitsright1"
+            height={2000}
+            width={2000}
+            className='w-[100px] xl:w-[230px] 2xl:w-[300px]'
+          />
+        </div>
+        {/* HERO MAIN PRODUCT IMAGE | CENTERED */}
         <div className='w-screen flex  flex-col justify-center items-center'>
-          <div className="text-center text-white mb-8 2xl:mt-10">
-            <h1 className="italic tracking-tight text-2xl xl:text-5xl 2xl:text-7xl">your healthy life</h1>
+          <div className="text-center text-white mb-8 xl:mb-8 2xl:mt-10">
+            <h1 className="italic tracking-tight text-4xl xl:text-5xl 2xl:text-7xl">your healthy life</h1>
             <div className='mt-1'>
               <p className='text-xl xl:text-3xl 2xl:text-4xl font-bold'>start here</p>
             </div>
@@ -25,11 +37,13 @@ const Hero = () => {
           <Image
             src="/assets/heroproducts.png"
             alt="products"
-            height={800}
-            width={800}
+            height={2000}
+            width={2000}
             className='xl:w-[800px] 2xl:w-[1200px]'
           />
         </div>
+
+        {/* HERO BOTTOM FRUIT */}
       </div>
     </>
   )
