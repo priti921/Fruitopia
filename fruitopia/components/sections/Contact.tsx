07c74ac7c24ebfa6dc51aacc744cc44f30179e34
Priti Roy <roypriti921@gmail.com>
2023-06-27 13:15:43 +0530

    font fix

--- fruitopia/components/sections/Contact.tsx
+++ fruitopia/components/sections/Contact.tsx
@@ -20,8 +20,8 @@ const Contact = () => {
       <div className='bg-yellow-100  absolute w-screen h-[400px] z-5 -mt-28 '></div>
       <div className='flex justify-between w-3/4 mx-auto my-10 font-[sans] relative z-10'>
         <div className='w-1/3'>
-          <p className='w-3/4 text-gray-600 font-thin text-sm my-2'>Get the latest news,events & more delivered to your box</p>
-          <input type='email' placeholder='Email address...' className='border-[0.1em] border-gray-400 rounded w-3/4 p-2 my-4 text-left text-sm ' />
+          <p className='w-3/4 text-gray-600 font-thin text-sm my-2'>Get the latest news, events & more delivered to your inbox</p>
+          <input type='email' placeholder='Email address...' className='border-[0.1px] border-gray-400 rounded w-3/4 p-2 my-4 text-left text-sm ' />
         </div>
         <div className='w-1/3 text-center text-medium  tracking-wider'>
           <div className='flex space-x-4 justify-center mb-5 '>
