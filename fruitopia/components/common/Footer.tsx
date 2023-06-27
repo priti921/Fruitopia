fced5f07db1224f6fc6a630f84c245702d7cf12a
Priti Roy <roypriti921@gmail.com>
2023-06-27 13:23:02 +0530

    fix

--- fruitopia/components/common/Footer.tsx
+++ fruitopia/components/common/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <section id='footer' className='relative z-10'>
       <div className='w-3/4 mx-auto font-[sans]'>
-        <hr className='border-t-2 border-gray-300' />
+        <hr className='border-t-[.1px] border-gray-300' />
         <p className='text-center font-thin text-sm my-5 text-gray-700'> Copyright &copy; <span className='font-bold'>Fruitopia</span> {new Date().getFullYear()}.All Rights Reserved</p>
       </div>
     </section>
